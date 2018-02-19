65adfd26a0b30fe99b99d10e964502e80daf7217
Xavi del Valle <xavi.delvalle@gmail.com>
2018-02-19 13:27:22 +0100

    45 server-tcp

---
+++ src/45 gorutinas 3 server_tcp/server_tcp.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		fmt.Println("Esperando conexion al puerto #8000")
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+
+		// manejarConexion(conn) // Manejamos una conexion a la vez.
+		go manejarConexion(conn) // Manejamos varias conexiones
+	}
+}
+
+func manejarConexion(conn net.Conn) {
+	defer conn.Close()
+
+	for {
+		_, err := io.WriteString(conn, time.Now().Format("15:04:05\n\r"))
+		if err != nil {
+			fmt.Println("Cliente desconectado!")
+			return // p ej. si el cliente de desconecta
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+}
