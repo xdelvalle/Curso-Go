eee46b598614151728e35f646e62d016f12e2f60
Xavi del Valle <xavi.delvalle@gmail.com>
2018-02-25 12:37:49 +0100

    Nombrado

--- src/web17 Templates/main.go
+++ src/web17 Templates/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"html/template"
+	"os"
+)
+
 // import (
 // 	"os"
 // 	"text/template"
@@ -69,44 +74,119 @@ package main
 // }
 
 // *********** IF *****************************************
-import (
-	"os"
-	"text/template"
-)
+// import (
+// 	"os"
+// 	"text/template"
+// )
 
-// El if sirve para el valor 0 de cada tipo .. int - 0, string - nil etc..
-const tp = `
+// // El if sirve para el valor 0 de cada tipo .. int - 0, string - nil etc..
+// const tp = `
+// {{range .}}
+// 	{{if .Edad}}
+// 		Nombre: {{.Nombre}} - Edad: {{.Edad}} - Correcto
+// 	{{else}}
+// 		Nombre: {{.Nombre}} - Edad: {{.Edad}} - INCORRECTO
+// 	{{end}}
+// {{end}}`
+
+// // Persona ..
+// type Persona struct {
+// 	Nombre string
+// 	Edad   int
+// }
+
+// func main() {
+// 	persona := []Persona{
+// 		{"Xavi", 38},
+// 		{"Pepe", 18},
+// 		{"Laia", 3},
+// 		{"Marc", 0},
+// 		{"Mia", 33},
+// 	}
+
+// 	t := template.New("persona")
+
+// 	t, err := t.Parse(tp)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	err = t.Execute(os.Stdout, persona)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
+
+// ****** NOMBRADOS ************************************
+// import (
+// 	"os"
+// 	"text/template"
+// )
+
+// // Persona ..
+// type Persona struct {
+// 	Nombre string
+// 	Edad   int
+// 	Pais   string
+// }
+
+// func mainX() {
+// 	persona := Persona{"Xavi", 38, "BCN"}
+// 	t := template.New("persona")
+
+// 	//t, err := t.ParseFiles("templates/residentes.txt","templates/visitantes.txt") //Los ficheros separados por comas
+// 	t, err := t.ParseGlob("templates/*.txt")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	err = t.ExecuteTemplate(os.Stdout, "visitantes", persona)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
+
+// ********* TEMPLATES CON WITH ************************
+const HERO = `
+Hero Name: {{.Name}}
+{{range .Emails}}
+Email: {{.}}
+{{end}}
+{{with .Friends}}
 {{range .}}
-	{{if .Edad}} 
-		Nombre: {{.Nombre}} - Edad: {{.Edad}} - Correcto
-	{{else}}
-		Nombre: {{.Nombre}} - Edad: {{.Edad}} - INCORRECTO
-	{{end}}
-{{end}}`
+Friend Name: {{.Name}}
+{{end}}
+{{end}}
+`
 
-// Persona ..
-type Persona struct {
-	Nombre string
-	Edad   int
+type Friend struct {
+	Name string
+}
+
+type Hero struct {
+	Name    string
+	Emails  []string
+	Friends []Friend
 }
 
 func main() {
-	persona := []Persona{
-		{"Xavi", 38},
-		{"Pepe", 18},
-		{"Laia", 3},
-		{"Marc", 0},
-		{"Mia", 33},
-	}
+	f1 := Friend{"Thor"}
+	f2 := Friend{"Hulk"}
 
-	t := template.New("persona")
+	t := template.New("Hero")
+	t, err := t.Parse(HERO)
 
-	t, err := t.Parse(tp)
 	if err != nil {
 		panic(err)
 	}
 
-	err = t.Execute(os.Stdout, persona)
+	hero := Hero{
+		Name:    "Ironman",
+		Emails:  []string{"ironmain@gmail.com", "turealironman@gmail.com"},
+		Friends: []Friend{f1, f2},
+	}
+
+	err = t.Execute(os.Stdout, hero)
 	if err != nil {
 		panic(err)
 	}
